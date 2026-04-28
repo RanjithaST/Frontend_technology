7202832d4d08dd777a70d3a91317365c60ea0a08
Ranjitha S T <ranjithast009@gmail.com>
2026-04-28 18:35:53 +0530

    JS

---
+++ s.js
@@ -0,0 +1,42 @@
+// let me = "Ranjitha"
+// console.log(me)
+// console.log(me)
+// console.log(me)
+// console.log(me)
+// console.log(me)
+// console.log(me)
+// console.log(me)
+// console.log(me)
+// console.log(me)
+// console.log(me)
+// console.log(me)
+// console.log(me)
+// console.log(me)
+// console.log(me)
+// console.log(me)
+// console.log(me)
+// console.log(me)
+// console.log(me)
+// console.log(me)
+// console.log(me)
+// console.log(me)
+// console.log(me)
+// console.log(me)
+// console.log("My name is",me)
+
+let first_name = "Ranjitha"
+let initial1 = "S"
+let initial2 = "T"
+let frd1 = "Rithika"
+let address1 = "KGF Kolar"
+let frd2 = "Chaitra"
+let address2 = "Raichur"
+let frd3 = "Sony"
+let address3 = "Raichur"
+let frd4 = "Lipika"
+let address4 = "Kodagu"
+let frd5 = "Dhanya"
+let address5 = "Hassan"
+console.log("Hello my name is", first_name, "\nMy friends: Friend 1: ", frd1,
+    "Address: ", address1, "Friend 2: ", frd2, "Address: ", address2, "Friend 3:", frd3, "Address: ", address3, "Friend : ", frd4, "Address: ", address4, "Friend 5: ", frd5, "Address: ", address5)
+console.log(`Hello I am ${first_name}.\nMy first friend name is ${frd1} and she lives in ${address1}.\nMy second friend name is ${frd2} and she lives in ${address2}.\nMy third friend name is ${frd3} and she lives in ${address3}.\nMy fourth friend name is ${frd4} and she lives in ${address4}\nMy last friend name is ${frd5} and she lives in ${address5}.\n`)
